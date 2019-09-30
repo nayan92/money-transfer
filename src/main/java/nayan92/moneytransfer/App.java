0739a65ea987663bf866f8c126b9f67371bf2d0b
Nayan Chudasama <nayan.chudasama11@gmail.com>
2019-09-30 19:41:58 +0100

    Added what I think the endpoints should be

--- src/main/java/nayan92/moneytransfer/App.java
+++ src/main/java/nayan92/moneytransfer/App.java
@@ -1,9 +1,14 @@
 package nayan92.moneytransfer;
 
-public class HelloWorld {
+public class App {
 
     public static void main(String[] args) {
+        new App().start();
+    }
+
+    public void start() {
         System.out.println("Hello World");
+        new Router().start();
     }
 
 }
